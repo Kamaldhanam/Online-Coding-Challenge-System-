5cf9bd65d5fa8a34248bb9644e34239b2dbe14c0
Kamal P <kamaldhanam888@gmail.com>
2025-10-17 20:21:38 +0530

    Create Problem.java

---
+++ Problem.java
@@ -0,0 +1,19 @@
+package coding_platform;
+
+public class Problem {
+    private String title;
+    private String description;
+
+    public Problem(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}

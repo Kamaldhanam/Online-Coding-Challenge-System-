18c3fabff4f9fc275da1cc275b772ddf1a555f14
Kamal P <kamaldhanam888@gmail.com>
2025-10-21 13:34:29 +0530

    Update SubmissionManager.java

--- SubmissionManager.java
+++ SubmissionManager.java
@@ -2,7 +2,7 @@ package coding_platform;
 
 import java.io.File;
 import java.sql.*;
-
+import java.util.*;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -31,7 +31,30 @@ public class SubmissionManager {
             e.printStackTrace();
         }
     }
+    public static int evaluateSubmission(String codeFilePath, String inputFile, String expectedOutputFile) throws Exception {
+    // 1. Compile user code
+    Process compile = new ProcessBuilder("javac", codeFilePath).start();
+    compile.waitFor();
 
+    // 2. Run code with input
+    Process run = new ProcessBuilder("java", "Main")
+        .redirectInput(new File(inputFile))
+        .redirectOutput(new File("user_output.txt"))
+        .start();
+    run.waitFor();
+
+    // 3. Compare output with expected
+    List<String> userOutput = Files.readAllLines(Paths.get("user_output.txt"));
+    List<String> expectedOutput = Files.readAllLines(Paths.get(expectedOutputFile));
+
+    if (userOutput.equals(expectedOutput)) {
+        return 100; // correct
+    } else {
+        return 0;   // wrong answer
+    }
+}
+    int score = evaluateSubmission("src/Main.java", "input.txt", "expected_output.txt");
+SubmissionManager.updateLeaderboard(username, problem.getTitle(), score);
     public static void updateLeaderboard(String username, String problemTitle, int score) {
         try {
             Connection conn = DriverManager.getConnection(

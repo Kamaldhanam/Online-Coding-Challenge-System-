2bf520110126e6e5ab6a93afecbd11d03f9a4f5b
Kamal P <kamaldhanam888@gmail.com>
2025-12-05 18:26:16 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,17 +14,6 @@ public class Main {
 
         System.out.println("Problem: " + problem.getTitle());
         System.out.println(problem.getDescription());
-
-        // Example DSA Solution: Reverse an array
-        int[] arr = {1, 2, 3, 4, 5};
-        System.out.print("Reversed array: ");
-        int score = 0;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            System.out.print(arr[i] + " ");
-            score += arr[i]; // Dummy score based on sum
-        }
-        System.out.println();
-
         // Update leaderboard and upload submission
         SubmissionManager.updateLeaderboard(username, problem.getTitle(), score);
         SubmissionManager.uploadToS3("src/Main.java", username, problem.getTitle());
